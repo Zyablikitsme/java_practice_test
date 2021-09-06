93d967c552e70b504451d535ff75dcca7095a570
Кирилл Бойко <v825ar@gmail.com>
2021-09-06 09:49:30 +0300

    Initial commit

---
+++ src/ru/mirea/java/practice2/Main.java
@@ -0,0 +1,11 @@
+package ru.mirea.java.practice2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++){
+            System.out.println("i=" + i);
+        }
+
+    }
+}
